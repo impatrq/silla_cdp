f4b1caa128c3cbe6ccff1031adfbdbb429f92c6e
Gian <giankeberlein@gmail.com>
2021-07-26 14:38:30 -0300

    Cambiar configuración de pines
    - Poner nuevo formato de dict de pines de motores.
    - Configurar los pines usando machine.Pin.
    - Arreglar llamada a return_to_default.

--- cdp_main.py
+++ cdp_main.py
@@ -1,4 +1,4 @@
-# import RPi.GPIO as gpio
+from machine import Pin
 import cdp_helper as helper
 import cdp_gui as gui
 import _thread as thr
@@ -7,37 +7,31 @@ import _thread as thr
 available_threads = {}
 
 # Variables de pines
-# Provisional...?
-pin_sensor = 32
+pin_sensor = Pin(32, Pin.IN, Pin.PULL_UP)
+# Solucion temporal hasta verificar una posibilidad de placa puente-h
 motor_pines = {
-    'pin_adelante' : 29,
-    'pin_atras' : 31,
-    'pin_enable_cabezal' : 33,
-    'pin_enable_apbrazo1' : 35,
-    'pin_enable_apbrazo2' : 37,
-    'pin_enable_lumbar' : 36,
-    'pin_enable_assdepth' : 38,
-    'pin_enable_assheight' : 40
+    'adelante' : [29],
+    'atras' : [31],
+    'cabezal' : [33],
+    'apbrazo' : [35, 37],
+    'lumbar' : [36],
+    'assdepth' : [38],
+    'assheight' : [40]
 }
 
 def set_motorpin_output():
-        for pin in motor_pines.values():
-            # gpio.setup(pin, gpio.OUT)
-            pass
-
-def Say(text):
-    print(text)
+    for key in motor_pines:
+        for pin in range(len(motor_pines[key])):
+            motor_pines[key][pin] = Pin(motor_pines[key][pin], Pin.OUT, Pin.PULL_DOWN, 0)
 
 def StartDefault():
     try:
         # Probablemente cambie en el futuro para albergar mas de un helper thread a la vez
-        available_threads['helper_thread'] = thr.start_new_thread(helper.return_to_default, (motor_pines['pin_atras'], pin_sensor))
+        available_threads['helper_thread'] = thr.start_new_thread(helper.return_to_default, (motor_pines['atras'], pin_sensor))
     except thr.error as e:
         print("[Err #001] Error al intentar iniciar un hilo.\n" + e)
 
 # Establecer entradas y salidas
-# gpio.setmode(gpio.BOARD)
-# gpio.setup(pin_sensor, gpio.IN)
 set_motorpin_output()
 
 if __name__ == "__main__":
