bbfa25df692e269285c65341106f7adcb0f0652f
Gian Keberlein <66274694+GianK128@users.noreply.github.com>
2021-07-28 15:43:29 -0300

    Feat: Cambiar listen-for-shutdown (#7)
    * Borrar listen-for-shutdown anterior
    
    * Agregar interrupcion listen-for-shutdown
    
    - Configurar interrupcion para pin con boton de apagado.
    - Hacer que la ESP32 entre en halt al ser disparada.

--- cdp_main.py
+++ cdp_main.py
@@ -1,3 +1,4 @@
+import machine
 from machine import Pin
 import cdp_helper as helper
 import cdp_gui as gui
@@ -8,6 +9,8 @@ available_threads = {}
 
 # Variables de pines
 pin_sensor = Pin(32, Pin.IN, Pin.PULL_UP)
+pin_off = Pin(9, Pin.IN, Pin.PULL_UP)       # Establecido como el que usabamos anteriormente (#TODO: cambiar pines)
+
 # Solucion temporal hasta verificar una posibilidad de placa puente-h
 motor_pines = {
     'adelante' : [29],
@@ -24,6 +27,15 @@ def set_motorpin_output():
         for pin in range(len(motor_pines[key])):
             motor_pines[key][pin] = Pin(motor_pines[key][pin], Pin.OUT, Pin.PULL_DOWN, 0)
 
+def listen_for_shutdown(pin: Pin):
+    # Dependiendo de como salgan las pruebas del reset, esta condicion podría moverse al principio del programa.
+    if machine.wake_cause() == machine.PIN_WAKE:
+        # TODO: Aca iría el codigo luego de despertar.
+        return
+    
+    # TODO: Acá iría el codigo previo a la suspensión...
+    machine.light_sleep()
+
 def StartDefault():
     try:
         # Probablemente cambie en el futuro para albergar mas de un helper thread a la vez
@@ -35,6 +47,10 @@ def StartDefault():
 set_motorpin_output()
 
 if __name__ == "__main__":
+    # Poner interrupción en el pin de apagado
+    pin_off.irq(handler = listen_for_shutdown, trigger = Pin.IRQ_FALLING, wake = machine.DEEPSLEEP or machine.IDLE)
+
+    # Correr modulo GUI en la primera pantalla
     available_threads['gui_thread'] = thr.start_new_thread(gui.AbrirVentana, ())
 
     # Los hilos no corren si se termina el programa principal (implementar luego detener hilos desde este script)
