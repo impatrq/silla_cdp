448f328110aa8b51f508a08aeda2db8f479d023d
agus-alfaro <agusalfaro128@gmail.com>
2021-08-30 09:54:30 -0300

    Revert "Considerar umbrales de sensores en calibracion"
    This reverts commit 8a3e096323d8b6bdd16765a27d9b8d9f52226ff2.
    
    Hice un push desde la cuenta equivocada, mala mia.

--- src/cdp_helper.py
+++ src/cdp_helper.py
@@ -47,9 +47,6 @@ def start_calibration(motor_pines: dict, sensor_pines: dict, turn_counter: Pin):
     for motor, pines in motor_pines.items():
         # Variable donde guardar la posicion
         pos = 0
-
-        # Variable con los valores del sensor
-        sensor_pin_list = sensor_pines[motor]
         
         # Mostrar instruccion
         gui.show_calib_instructions(motor)
@@ -59,7 +56,7 @@ def start_calibration(motor_pines: dict, sensor_pines: dict, turn_counter: Pin):
             pin.value(1)
 
         # Hasta que no este en la posicion correcta...
-        while not adc_check_threshold(sensor_pin_list[0], *sensor_pin_list[2:]):
+        while not adc_check_threshold(sensor_pines[motor]):
             # Contar pulsos/vueltas del motor
             if turn_counter.value() == 1:
                 pos += 1
