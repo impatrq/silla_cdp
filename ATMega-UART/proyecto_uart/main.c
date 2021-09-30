9c17b45a140c08611b6541d15aa8932755f062bb
Gian <giankeberlein@gmail.com>
2021-09-30 15:25:11 -0300

    Agregar proyecto Atmel Studio (UART desde ATMega)

---
+++ ATMega-UART/proyecto_uart/main.c
@@ -0,0 +1,146 @@
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdio.h>
+#include "uart_hal.h"
+#include "adc_hal.h"
+
+uint8_t print_buffer[64] = {};
+
+typedef enum {
+	NONE,
+	BAR,
+	LUM1,
+	LUM2,
+	LUM3,
+	LUM4,
+	ASS1,
+	ASS2,
+	APB	
+} ASK;
+
+// ¿Para facilitar la lectura?
+ASK ask_value_to_enum(char* ask_value) {
+	if (strncmp(ask_value, "bar", 3) == 0) {
+		return BAR;
+	}
+	else if (strncmp(ask_value, "lu1", 3) == 0) {
+		return LUM1;
+	}
+	else if (strncmp(ask_value, "lu2", 3) == 0) {
+		return LUM2;
+	}
+	else if (strncmp(ask_value, "lu3", 3) == 0) {
+		return LUM3;
+	}
+	else if (strncmp(ask_value, "lu4", 3) == 0) {
+		return LUM4;
+	}
+	else if (strncmp(ask_value, "as1", 3) == 0) {
+		return ASS1;
+	}
+	else if (strncmp(ask_value, "as2", 3) == 0) {
+		return ASS2;
+	}
+	else if (strncmp(ask_value, "apb", 3) == 0) {
+		return APB;
+	}
+	else {
+		return NONE;
+	}
+}
+
+int main(void)
+{
+	uint16_t len = 6;
+	uint16_t convert = 0;
+	uint8_t reading[len];
+	uint8_t ask_value[len/2];
+	uint8_t pin_value = 0;
+	
+	ASK ask_state = 0;
+	
+	// Inicializar PORTB
+	DDRB = 0x00;
+	
+	// Inicializar UART (baudrate = 9600, single)
+	uart_init(9600, 0);
+	
+	// Inicializar pines ADC
+	adc_init();
+	adc_pin_enable(ADC0_PIN);
+	adc_pin_enable(ADC1_PIN);
+	adc_pin_enable(ADC2_PIN);
+	adc_pin_enable(ADC3_PIN);
+	adc_pin_enable(ADC4_PIN);
+	adc_pin_enable(ADC5_PIN);
+	
+	// Habilitar interrupciones
+	sei();
+	
+    while (1) 
+    {
+		// Esperar lectura de 6 bytes
+		uart_wait_read_string(reading, len);
+		
+		// Entrar si los primeros 3 bytes son "ask"
+		if (strncmp((char*)reading, "ask", 3) == 0) {
+			// Meter en ask_value los ultimos 3 bytes
+			sprintf((char*)ask_value, (char*)reading + 3);
+			
+			// Limpiar print_buffer y convertir ask_value a una int (para usar un switch case)
+			memset(print_buffer, 0, sizeof(print_buffer));
+			ask_state = ask_value_to_enum((char*)ask_value);
+			
+			switch (ask_state) {
+				// Estos dos son pines digitales, vienen de una compuerta AND.
+				case BAR: pin_value = (PINB & (1 << PINB2)) >> PINB2; sprintf((char*)print_buffer, "%d", pin_value); break;
+				case APB: pin_value = (PINB & (1 << PINB3)) >> PINB3; sprintf((char*)print_buffer, "%d", pin_value); break;
+				
+				// Los que siguen son de entradas analogicas, vienen directo de los piezoelectricos.
+				case LUM1: 
+					adc_pin_select(ADC0_PIN);
+					convert = adc_convert();
+					sprintf((char*)print_buffer, "%u", convert);
+				break;
+				case LUM2:
+					adc_pin_select(ADC1_PIN);
+					convert = adc_convert();
+					sprintf((char*)print_buffer, "%u", convert);
+				break;
+				case LUM3:
+					adc_pin_select(ADC2_PIN);
+					convert = adc_convert();
+					sprintf((char*)print_buffer, "%u", convert);
+				break;
+				case LUM4:
+					adc_pin_select(ADC3_PIN);
+					convert = adc_convert();
+					sprintf((char*)print_buffer, "%u", convert);
+				break;
+				case ASS1:
+					adc_pin_select(ADC4_PIN);
+					convert = adc_convert();
+					sprintf((char*)print_buffer, "%u", convert);
+				break;
+				case ASS2:
+					adc_pin_select(ADC5_PIN);
+					convert = adc_convert();
+					sprintf((char*)print_buffer, "%u", convert);
+				break;
+				
+				// En caso que no se haya pedido correctamente.
+				default: sprintf((char*)print_buffer, "ERR"); break;
+			}
+			
+			// Enviar print_buffer
+			uart_send_string(print_buffer);
+		}
+    }
+}
+
---
+++ ATMega-UART/proyecto_uart/uart_hal.c
@@ -0,0 +1,96 @@
+#include "uart_hal.h"
+
+volatile static uint8_t rx_buffer[RX_BUFFER_SIZE] = {0};
+volatile static uint16_t rx_count = 0;
+volatile static uint8_t uart_tx_busy = 1;
+
+ISR(USART_RX_vect) {
+	volatile static uint16_t rx_write_pos = 0;
+	
+	rx_buffer[rx_write_pos] = UDR0;
+	rx_count++;
+	rx_write_pos++;
+	if (rx_write_pos >= RX_BUFFER_SIZE) {
+		rx_write_pos = 0;
+	}
+}
+
+ISR(USART_TX_vect) {
+	uart_tx_busy = 1;
+}
+
+void uart_init(uint32_t baudrate, uint8_t double_speed) {
+	uint8_t speed = 16;
+	
+	if (double_speed != 0) {
+		speed = 8;
+		UCSR0A |= 1 << U2X0;
+	}
+	
+	baudrate = (F_CPU/(speed*baudrate)) - 1;
+	
+	UBRR0H = (uint8_t)(baudrate >> 8);
+	UBRR0L = (uint8_t)(baudrate & 0xFF);
+	
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << TXCIE0) | (1 << RXCIE0);
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+// Enviar un caracter
+void uart_send_byte(uint8_t c) {
+	while(uart_tx_busy == 0);
+	uart_tx_busy = 0;
+	UDR0 = c;
+}
+
+// Enviar caracteres
+void uart_send_array(uint8_t *c, uint16_t len) {
+	for (uint16_t i = 0; i < len; i++) {
+		uart_send_byte(c[i]);
+	}
+}
+
+// Enviar una string
+void uart_send_string(uint8_t *c) {
+	uint16_t i = 0;
+	
+	do {
+		uart_send_byte(c[i]);
+		i++;
+	} while (c[i] != '\0');
+	
+	uart_send_byte(c[i]);
+}
+
+// Leer si hay datos en el buffer
+uint16_t uart_read_count(void) {
+	return rx_count;
+}
+
+// Leer un byte del buffer
+uint8_t uart_read(void) {
+	static uint16_t rx_read_pos = 0;
+	uint8_t data = 0;
+	
+	data = rx_buffer[rx_read_pos];
+	rx_read_pos++;
+	rx_count--;
+	if (rx_read_pos >= RX_BUFFER_SIZE) {
+		rx_read_pos = 0;
+	}
+	
+	return data;
+}
+
+// Leer 'len' bytes del buffer
+void uart_wait_read_string(uint8_t* str, uint16_t len) {
+	uint16_t i = 0;
+	
+	do {
+		if (uart_read_count() > 0) {
+			uint8_t c = uart_read();
+			str[i] = c;
+			i++;
+		}
+	} while (i < len);
+}
