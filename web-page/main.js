49bff43b0e5b573f2b3a70a30b583ee8605ff307
Gian <giankeberlein@gmail.com>
2021-10-21 17:34:11 -0300

    Cerrar hamburger menu si se scrollea

--- web-page/main.js
+++ web-page/main.js
@@ -4,18 +4,6 @@ topButton = document.getElementById('btnScrollTop');
 
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topButton.style.display = 'block';
-    } else {
-        topButton.style.display = 'none';
-    }
-}
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
 // MANEJAR NAV RESPONSIVE
 const hamburger = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.mobile-nav');
@@ -33,3 +21,20 @@ hamburger.addEventListener('click', button => {
         menuOpen = false;
     }
 });
+
+// Funciones
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topButton.style.display = 'block';
+        hamburger.classList.remove('is-active');
+        mobileNav.classList.remove('is-active');
+        hamburger.classList.remove('open');
+        menuOpen = false;
+    } else {
+        topButton.style.display = 'none';
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
