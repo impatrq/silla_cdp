32fa7bc7481a2b38f1955c529a41fc31fb7a86d8
Gian <giankeberlein@gmail.com>
2021-10-21 11:44:10 -0300

    Agregar visualizador de modelos

---
+++ web-page/models.js
@@ -0,0 +1,23 @@
+const carouselImages = document.querySelector('.carousel__images');
+const carouselButtons = document.querySelectorAll('.carousel__button');
+const imagesNumber = document.querySelectorAll('.carousel__images img').length;
+let imageIndex = 1;
+let translateX = 0;
+
+carouselButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        if (event.target.id === 'previous') {
+            if (imageIndex !== 1) {
+                imageIndex--;
+                translateX += 300;
+            }
+        } else {
+            if (imageIndex !== imagesNumber) {
+                imageIndex++;
+                translateX -= 300;
+            }
+        }
+
+        carouselImages.style.transform = `translateX(${translateX}px)`;
+    });
+});
