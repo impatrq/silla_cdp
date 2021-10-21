a58f67acba60bc15ec7485b9651e8c18a730b6c2
Gian <giankeberlein@gmail.com>
2021-10-21 17:14:04 -0300

    Añadir responsive a pagina de modelos

--- web-page/main.js
+++ web-page/main.js
@@ -1,3 +1,5 @@
+
+// BOTON PARA SCROLL HACIA ARRIBA
 topButton = document.getElementById('btnScrollTop');
 
 window.onscroll = function() {scrollFunction()};
@@ -9,8 +11,25 @@ function scrollFunction() {
         topButton.style.display = 'none';
     }
 }
-
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// MANEJAR NAV RESPONSIVE
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
+let menuOpen = false;
+
+hamburger.addEventListener('click', button => {
+    hamburger.classList.toggle('is-active');
+    mobileNav.classList.toggle('is-active');
+
+    if (!menuOpen) {
+        hamburger.classList.add('open');
+        menuOpen = true;
+    } else {
+        hamburger.classList.remove('open');
+        menuOpen = false;
+    }
+});
