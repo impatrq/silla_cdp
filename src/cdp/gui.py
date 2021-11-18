6e0f3bc8c35f0588418392edddfe882ed3ac87fa
jonathangalotto <jonathan.galotto@yahoo.com>
2021-11-18 15:38:01 -0300

    Registrar input device joystick lvgl

--- src/cdp/gui.py
+++ src/cdp/gui.py
@@ -1,6 +1,28 @@
-# Modulo de interfaz de usuario
-# Por ahora, estan solo las funciones necesarias para que no tire error, hasta tener lvgl andando
+import lvgl as lv
+from cdp import uart
 
+# ===== CALLBACKS ===== #
+def read_joystick_cb(drv, data):
+    read = uart.send_and_receive("askswi").split('-')
+
+    if read[0] > 900:
+        print('Right')
+        data.key = lv.KEY.RIGHT
+    elif read[0] < 100:
+        print('Left')
+        data.key = lv.KEY.LEFT
+    else:
+        data.key = 0
+
+    if read[2] == 0:
+        print('Pressed')
+        data.state = lv.INDEV_STATE.PRESSED
+    else:
+        data.state = lv.INDEV_STATE.RELEASED
+
+    return False
+
+# ===== FUNCIONES ===== #
 def show_calib_instructions(which: str):
     print(which)
 
