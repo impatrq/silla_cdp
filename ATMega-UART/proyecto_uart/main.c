1703f431828762aec378cb8932544c3dbda318ae
Facunditenn <82180551+Facunditenn@users.noreply.github.com>
2021-11-30 00:59:21 -0300

    Fix: Errores de lectura del ATMega328P (#37)
    * Cambiar inicializacion de pines
    
    * Arreglar limpieza y envio de UART buffer
    
    * Aumentar baudrate de UART a 76800
    
    * Devolver baudrate a sus valores anteriores
    
    El problema no era debido al baudrate, ya que persiste igualmente.
    
    * Agregar caracter nulo necesario a final de lectura
    
    * Agregar eco al no ser un valor de ASK ni de MUX
    
    * Agregar nueva build para el ATMega (.hex, .elf)
    
    * Agregar correccion de UART a funcion dummy_read
    
    * Hacer correccion UART antes de arrancar programa

--- ATMega-UART/proyecto_uart/main.c
+++ ATMega-UART/proyecto_uart/main.c
@@ -1,6 +1,4 @@
-#ifndef F_CPU
 #define F_CPU 16000000UL
-#endif
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -23,7 +21,7 @@ typedef enum {
 	SWITCH
 } ASK;
 
-// �Para facilitar la lectura?
+// Para facilitar la lectura?
 ASK ask_value_to_enum(char* ask_value) {
 	if (strncmp(ask_value, "bar", 3) == 0) {
 		return BAR;
@@ -61,8 +59,9 @@ int main(void)
 	
 	ASK ask_state = 0;
 	
-	// Inicializar PORTB
+	// Inicializar pines
 	DDRB = 0x02;	// PB1 como salida
+	PORTB =	0x10;	// PB4 activar pull-up
 	DDRD = 0x60;	// PD5, PD6 como salida
 	
 	// Inicializar UART (baudrate = 9600, single)
@@ -80,8 +79,8 @@ int main(void)
 	// Habilitar interrupciones
 	sei();
 	
-    while (1) 
-    {
+	while (1)
+	{		
 		// Esperar lectura de 6 bytes
 		uart_wait_read_string(reading, len);
 		
@@ -91,7 +90,6 @@ int main(void)
 			sprintf((char*)ask_value, (char*)reading + 3);
 			
 			// Limpiar print_buffer y convertir ask_value a una int (para usar un switch case)
-			memset(print_buffer, 0, sizeof(print_buffer));
 			ask_state = ask_value_to_enum((char*)ask_value);
 			
 			switch (ask_state) {
@@ -100,7 +98,7 @@ int main(void)
 				case APB: pin_value = (PINB & (1 << PINB3)) >> PINB3; sprintf((char*)print_buffer, "%d", pin_value); break;
 				
 				// Los que siguen son de entradas analogicas, vienen directo de los piezoelectricos.
-				case LUM1: 
+				case LUM1:
 					adc_pin_select(ADC0_PIN);
 					convert = adc_convert();
 					sprintf((char*)print_buffer, "%u", convert);
@@ -137,18 +135,12 @@ int main(void)
 				break;
 				
 				// En caso que no se haya pedido correctamente.
-				default: sprintf((char*)print_buffer, "ERR"); break;
+				default: sprintf((char*)print_buffer, "ASKERR"); break;
 			}
-			
-			// Enviar print_buffer
-			uart_send_string(print_buffer);
 		}
 		else if (strncmp((char*)reading, "mux", 3) == 0) {
 			// Meter en ask_value los ultimos 3 bytes
 			sprintf((char*)ask_value, (char*)reading + 3);
-			
-			// Limpiar print_buffer
-			memset(print_buffer, 0, sizeof(print_buffer));
 			
 			unsigned int i = 0;
 			
@@ -165,10 +157,15 @@ int main(void)
 			i = ask_value[2] - '0';
 			PORTD ^= (-i ^ PORTD) & (1 << PIND5);
 			
-			// Enviar mensaje de confirmaci�n
-			sprintf((char*)print_buffer, "ADDRSET");
-			uart_send_string(print_buffer);
+			// Enviar mensaje de confirmacion
+			sprintf((char*)print_buffer, "MUXSET");
 		}
-    }
+		else {
+			// Hacer eco del mensaje
+			sprintf((char*)print_buffer, reading);
+		}
+		
+		// Enviar print_buffer
+		uart_send_string(print_buffer);
+	}
 }
-
